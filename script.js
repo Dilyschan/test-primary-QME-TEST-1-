953f09b1211ee9bfd4294a3cb36f1d18caed6afe
Dilyschan <117697687+Dilyschan@users.noreply.github.com>
2026-04-15 15:22:17 +0700

    Fix assignment operator in sendToSheet function

--- script.js
+++ script.js
@@ -655,7 +655,7 @@ function createConfetti() {
 // ===== SEND TO GOOGLE SHEET =====
 function sendToSheet(data) {
     // If URL not configured, save locally
-    if (GOOGLE_SHEET_URL === 'https://script.google.com/macros/s/AKfycbzj6LZz7KAM57f9xb77cZnUtrEbBznDefCJGHwTGw63zsoxY1RU1SRF4mE2f7af3LXB/exec') {
+    if (GOOGLE_SHEET_URL ='https://script.google.com/macros/s/AKfycbzj6LZz7KAM57f9xb77cZnUtrEbBznDefCJGHwTGw63zsoxY1RU1SRF4mE2f7af3LXB/exec') {
         console.log('📊 Data to send to Google Sheet:', data);
         // Save to localStorage as fallback
         const existingData = JSON.parse(localStorage.getItem('examResults') || '[]');
